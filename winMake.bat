76a1dd9545bbb3800bedba9b1a040dfae477eb2e
B0RJA <borja.latorre@eead.csic.es>
2012-05-14 10:08:50 +0300

    Now checks if gmake is installed

--- winMake.bat
+++ winMake.bat
@@ -1,3 +1,16 @@
+@echo off
 gmake
-pause
 
+if errorlevel 3 goto error
+goto end:
+
+:error
+echo.
+echo Error: gmake not found
+echo.
+echo Install Strawberry Perl from:
+echo   http://strawberryperl.com/
+echo.
+
+:end
+pause
